8cab5757d216aacf7235925b662cf30463a6b6ab
Rayngumi <rayngumi25@gmail.com>
2024-03-19 20:09:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'John Cena';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser.charAt(0);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
